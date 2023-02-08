dd289c3a1ecbc35b065a0d1539eed0aa5ede7728
druksx <aurelien.leclercq@epitech.eu>
2023-02-08 14:26:34 +0100

    [ADD] 1.lua

---
+++ 1.lua
@@ -0,0 +1,11 @@
+for a = 1, 100 do
+    if (a % 5 == 0 and a % 3 ~= 0)then
+        print("Five")
+    end
+    if (a % 3 == 0 and a % 5 ~= 0) then
+        print("Three")
+    end
+    if (a % 3 == 0 and a % 5 == 0) then
+        print("ThreeFive")
+    end
+end
