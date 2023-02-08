d064ccdec8e5546e298b56a0e8f163d3bb682a3b
druksx <aurelien.leclercq@epitech.eu>
2023-02-08 16:10:55 +0100

    [ADD] Error handling in 3.lua for EX04.

--- 3.lua
+++ 3.lua
@@ -1,7 +1,12 @@
 function add(...)
     result = 0
     for k, v in pairs{...} do
-        result = result + v
+        if type(v) == "number" then
+            result = result + v
+        else
+            print("Something went wrong. Please try to use valid parameters.")
+            return
+        end
     end
     print(result)
 end
